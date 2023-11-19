4ab90632aa52a9fa041096f5ecf728e7db9da6f4
Reez mg <141248542+reezevo@users.noreply.github.com>
2023-11-19 21:14:03 +0000

    dd

--- index.js
+++ index.js
@@ -4,9 +4,27 @@ const axios = require('axios');
 const { Configuration, OpenAIApi } = require("openai");
 require('dotenv').config();
 const app = express().use(body_parser.json());
-
+const token = process.env.TOKEN;
+const mytoken=process.env.MYTOKEN;
 const PORT = process.env.PORT || 8800;
+app.get("/webhook",(req,res)=>{
+  let mode = req.query["hub.mode"];
+  let challenge = req.query["hub.challenge"];
+  let tokens = req.query["hub.verify_token"];
 
+  
+
+  if(mode && token){
+      if(mode === "subscribe" && tokens === mytoken){
+          res.status(200).send(challenge);
+          console.log("reez is on")
+      }else{
+          res.status(400)
+      }
+  }
+
+  
+});
 
 app.get('/', (req, res) => {
     res.send(`
